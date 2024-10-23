3542cc5d9cbc11f1f0592b100a833ab140911057
WetHorse <s26850@pjwstk.edu.pl>
2024-10-23 19:56:59 +0200

    Add new UnitTest Project with data validation :)

---
+++ BYT_Project/BYT_Project_UnitTests/UnitTest1.cs
@@ -0,0 +1,53 @@
+using BYT_Project.Model;
+
+namespace BYT_Project_UnitTests;
+
+
+public class Tests
+{
+    
+    Employee employee = new Support(1000, "Senior", new DateTime(2021, 06, 21), "Mike", "Wazowski",
+        new DateTime(1989, 06, 11), new DateTime(2021, 06, 22));
+    
+    [Test]
+    public void EmployeeDataValidationTest_Salary()
+    {
+        Assert.IsInstanceOf<int>(employee.Salary);
+    }
+    
+    [Test]
+    public void EmployeeDataValidationTest_Experience()
+    {
+        Assert.IsInstanceOf<string>(employee.Experience);
+    }
+    
+    [Test]
+    public void EmployeeDataValidationTest_DateOfEmployment()
+    {
+        Assert.IsInstanceOf<DateTime>(employee.DateOfEmployment);
+    }
+    
+    [Test]
+    public void EmployeeDataValidationTest_Name()
+    {
+        Assert.IsInstanceOf<string>(employee.Name);
+    }
+    
+    [Test]
+    public void EmployeeDataValidationTest_Surname()
+    {
+        Assert.IsInstanceOf<string>(employee.Surname);
+    }
+    
+    [Test]
+    public void EmployeeDataValidationTest_DateOfBirth()
+    {
+        Assert.IsInstanceOf<DateTime>(employee.DateOfBirth);
+    }
+    
+    [Test]
+    public void EmployeeDataValidationTest_CreatedTime()
+    {
+        Assert.IsInstanceOf<DateTime>(employee.CreatedAt);
+    }
+}
