a171277aff1d675b376a0cea2be069bd9caf84b2
Oleksandr-Marych <112111959+AlexMarych@users.noreply.github.com>
2024-10-24 12:45:33 +0200

    Tests added

---
+++ BYT_Project/BYT_Project_UnitTests/LevelTest.cs
@@ -0,0 +1,20 @@
+﻿using BYT_Project.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BYT_Project_UnitTests
+{
+    internal class LevelTest
+    {
+        Level level = new Level(Level.Name.Advanced);
+
+        [Test]
+        public void LevelDataValidationTest_Name()
+        {
+            Assert.IsInstanceOf<Enum>(level.name);
+        }
+    }
+}
---
+++ BYT_Project/BYT_Project_UnitTests/PetitionTest.cs
@@ -0,0 +1,26 @@
+﻿using BYT_Project.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BYT_Project_UnitTests
+{
+    internal class PetitionTest
+    {
+        Petition petition = new Petition("petition", Petition.StatusType.Opened);
+
+        [Test]
+        public void PetitionDataValidationTest_Text()
+        {
+            Assert.IsInstanceOf<string>(petition.Text);
+        }
+        [Test]
+        public void PetitionDataValidationTest_Status()
+        {
+            Assert.IsInstanceOf<Enum>(petition.Status);
+        }
+    }
+}
