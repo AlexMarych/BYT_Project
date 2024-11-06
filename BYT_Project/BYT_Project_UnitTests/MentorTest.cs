9f39aca997f4b4ad58c8136c3f0f046a46b5c4d0
WetHorse <s26850@pjwstk.edu.pl>
2024-11-06 13:38:00 +0100

    Added unit tests
    MentorTest
    PaymentTest
    Added unit tests

--- BYT_Project/BYT_Project_UnitTests/MentorTest.cs
+++ BYT_Project/BYT_Project_UnitTests/MentorTest.cs
@@ -13,9 +13,52 @@ namespace BYT_Project_UnitTests
         new DateTime(1989, 06, 11), new DateTime(2021, 06, 22), "spec");
 
         [Test]
+        public void MentorDataValidationTest_Salary()
+        {
+            Assert.IsInstanceOf<int>(mentor.Salary);
+        }
+        
+        [Test]
+        public void MentorDataValidationTest_Experience()
+        {
+            Assert.IsInstanceOf<string>(mentor.Experience);
+        }
+        
+        [Test]
+        public void MentorDataValidationTest_DateOfEmp()
+        {
+            Assert.IsInstanceOf<DateTime>(mentor.DateOfEmployment);
+        }
+        
+        [Test]
+        public void MentorDataValidationTest_Name()
+        {
+            Assert.IsInstanceOf<string>(mentor.Name);
+        }
+        
+        [Test]
+        public void MentorDataValidationTest_Surname()
+        {
+            Assert.IsInstanceOf<string>(mentor.Surname);
+        }
+        
+        [Test]
+        public void MentorDataValidationTest_DateOfBirth()
+        {
+            Assert.IsInstanceOf<DateTime>(mentor.DateOfBirth);
+        }
+        
+        [Test]
+        public void MentorDataValidationTest_CreatedAt()
+        {
+            Assert.IsInstanceOf<DateTime>(mentor.CreatedAt);
+        }
+        
+        [Test]
         public void MentorDataValidationTest_Specialization()
         {
             Assert.IsInstanceOf<string>(mentor.Specialization);
         }
+        
     }
 }
--- BYT_Project/BYT_Project_UnitTests/PaymentTest.cs
+++ BYT_Project/BYT_Project_UnitTests/PaymentTest.cs
@@ -1,6 +1,38 @@
+using BYT_Project.Model;
+
 namespace BYT_Project_UnitTests;
 
 public class PaymentTest
 {
+    private static Student student =
+        new Student("Mike", "Wazowski", new DateTime(2003, 07, 21), 
+            new DateTime(2020, 08, 11), 12000, 4);
+    
+    static IDictionary<string, Mentor> role = new Dictionary<string, Mentor>();
+
+    static List<Test>? questions = new List<Test>();
+    
+    private static Course course = new Managment("name", 20000, role, Course.DifficultyLevel.Advanced, questions);
+
+    private Payment payment = new Payment(new DateTime(2023, 11, 11), student, course);
+
+    [Test]
+    public void PaymentDataValidation_PaymentDate()
+    {
+        Assert.IsInstanceOf<DateTime>(payment.PaymentDate);
+    }
+    
+    [Test]
+    public void PaymentDataValidation_StudentObject()
+    {
+        Assert.IsInstanceOf<Student>(payment.Student);
+    }
+    
+    [Test]
+    public void PaymentDataValidation_CourseObject()
+    {
+        Assert.IsInstanceOf<Course>(payment.Course);
+    }
+    
     
 }
