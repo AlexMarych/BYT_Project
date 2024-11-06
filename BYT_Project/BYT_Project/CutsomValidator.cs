90b6d231b351bbeb0acd88f819fab20821b66d44
invi <roflankaa@gmail.com>
2024-11-06 13:40:20 +0100

    changes

---
+++ BYT_Project/BYT_Project/CutsomValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BYT_Project
+{
+    public class CutsomValidator
+    {
+        public static void Validate<T>(T obj)
+        {
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
+
+            var context = new ValidationContext(obj);
+            var validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, context, validationResults, true);
+
+            if (!isValid)
+            {
+                var errorMessages = string.Join("; ", validationResults.Select(vr => vr.ErrorMessage));
+                throw new ValidationException($"Validation failed: {errorMessages}");
+            }
+        }
+    }
+}
