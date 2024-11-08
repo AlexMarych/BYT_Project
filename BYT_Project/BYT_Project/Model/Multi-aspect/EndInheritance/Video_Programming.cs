3a91d319d8839526ed858c358cb13072e1593516
Oleksandr-Marych <112111959+AlexMarych@users.noreply.github.com>
2024-11-08 22:12:00 +0100

    Merge conflic solve

---
+++ BYT_Project/BYT_Project/Model/Multi-aspect/EndInheritance/Video_Programming.cs
@@ -0,0 +1,32 @@
+﻿using BYT_Project.Utils;
+using System.ComponentModel.DataAnnotations;
+
+namespace BYT_Project.Model
+{
+    public class Video_Programming : Programming
+    {
+        public TimeSpan OverallDuration { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int VideosNumber { get; set; }
+
+        private static List<Video_Programming> _extent = [];
+
+        static Video_Programming()
+        {
+            _extent = ExtentManager.LoadExtent<Video_Programming>();
+        }
+
+        public Video_Programming(TimeSpan overallDuration, int videosNumber, string technologyName, List<string> frameworkList, string name, int price, IDictionary<string, Mentor>? mentors, DifficultyLevel level, List<Test>? questions) : base(technologyName, frameworkList, name, price, mentors, level, questions)
+        {
+            OverallDuration = overallDuration;
+            VideosNumber = videosNumber;
+
+            CutsomValidator.Validate(this);
+
+            _extent.Add(this);
+            ExtentManager.ClearExtent<Video_Programming>();
+            ExtentManager.SaveExtent(_extent);
+        }
+    }
+}
